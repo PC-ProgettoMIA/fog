9d3add8b4995da85078e667310c526bbfd6dcc13
Matteo Scucchia <matteo.scucchia@studio.unibo.it>
2022-01-12 11:50:58 +0100

    Solved password and ssl problem in fog

--- fog/server/src/main/java/server/Server.java
+++ fog/server/src/main/java/server/Server.java
@@ -2,28 +2,57 @@ package server;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.JksOptions;
 import io.vertx.ext.mongo.MongoClient;
 import server.controllers.*;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.security.Key;
+import java.util.Base64;
 
 /**
  * Server of fog.
  */
 public class Server extends AbstractVerticle {
 
+    private static final String PATH_TO_RESOURCES = "server/src/main/resources";
+    private static final String ENCRYPTION_PROTOCOL = "AES/ECB/PKCS5PADDING";
+
     private final int localPort;
-
     private Routes routes;
-
+    
     public Server(final int localPort) {
         this.localPort = localPort;
     }
 
     @Override
-    public void start() throws Exception {
+    public void start() {
         initialize();
-        getVertx().createHttpServer().requestHandler(this.routes.getRouter()).listen(localPort);
-        System.out.println("Server online on port " + localPort);
+        try {
+            String password = this.decryptPassword();
+            getVertx().createHttpServer(
+
+                    new HttpServerOptions()
+                            .setSsl(true)
+                            .setKeyStoreOptions(new JksOptions()
+                                    .setPath(this.getUserDir() +
+                                            this.getFileSeparator() +
+                                            PATH_TO_RESOURCES +
+                                            "/keystore.jks")
+                                    .setPassword(password))
+            ).requestHandler(this.routes.getRouter()).listen(localPort);
+            System.out.println("Server online on port " + localPort);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void initialize() {
@@ -47,5 +76,36 @@ public class Server extends AbstractVerticle {
         AbstractVerticle service = new Server(3001);
         Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(service);
+    }
+
+    private String decryptPassword() throws Exception {
+        BufferedReader keyReader = keyReader = new BufferedReader(
+                new FileReader(this.getUserDir() +
+                        this.getFileSeparator() +
+                        PATH_TO_RESOURCES +
+                        "/key.txt"));
+        String key = keyReader.readLine();
+        keyReader.close();
+        BufferedReader passwordReader = new BufferedReader(
+                new FileReader(this.getUserDir() +
+                        this.getFileSeparator() +
+                        PATH_TO_RESOURCES +
+                        "/password.txt"));
+        String encryptedPassword = passwordReader.readLine();
+        passwordReader.close();
+
+        Cipher cipher = Cipher.getInstance(ENCRYPTION_PROTOCOL);
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedPassword)));
+
+    }
+
+    private String getUserDir() {
+        return System.getProperty("user.dir");
+    }
+
+    private String getFileSeparator() {
+        return System.getProperty("file.separator");
     }
 }
